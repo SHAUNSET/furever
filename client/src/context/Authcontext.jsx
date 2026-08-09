0636aa5ca13c56bdf1fa44635131512e2c3fba33
Shaunak Naik <shaunak206107@gmail.com>
2026-08-09 19:41:15 +0530

    Update server URL to production endpoint

--- client/src/context/Authcontext.jsx
+++ client/src/context/Authcontext.jsx
@@ -4,7 +4,7 @@ import React, { createContext } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-    let serverUrl = "http://localhost:8000";
+    let serverUrl = "https://furever-server.onrender.com";
 
     let value = {
         serverUrl
